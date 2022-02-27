bf4d20d6a25584cc1bbc8e34d788d8e1fa18fa24
Håkan Thörngren <hth313@gmail.com>
2022-02-26 17:22:29 -0800

    Add a simplified linker control file for A2560U

---
+++ linker-files/a2560u-simplified.scm
@@ -0,0 +1,5 @@
+(define memories
+  '((memory flash (address (#x12000 . #x1fffff)) (type ROM))
+    (memory dataRAM (address (#x200000 . #x37ffff)) (type RAM))
+    (memory Vector (address (#x0000 . #x03ff)))
+    ))
