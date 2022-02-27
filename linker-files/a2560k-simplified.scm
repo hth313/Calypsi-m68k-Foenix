3f7d7730813aa0adbd84402101bd938d3590685f
Håkan Thörngren <hth313@gmail.com>
2022-02-27 11:31:55 -0800

    Add a startup for morfe and A2560K rules

---
+++ linker-files/a2560k-simplified.scm
@@ -0,0 +1,5 @@
+(define memories
+  '((memory flash (address (#x200000 . #x2fffff)) (type ROM))
+    (memory dataRAM (address (#x300000 . #x37ffff)) (type RAM))
+    (memory Vector (address (#x0000 . #x03ff)))
+    ))
