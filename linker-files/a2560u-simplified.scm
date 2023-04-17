5ab7114337cbe3368cb8a537dd6e337ffd56d0f3
Håkan Thörngren <hth313@gmail.com>
2023-04-16 22:46:05 -0700

    Revise linker rule files
    Use more appropriate names. Add palette and vram to the a2560u+ rules
    file.

--- linker-files/a2560u-simplified.scm
+++
@@ -1,5 +0,0 @@
-(define memories
-  '((memory flash (address (#x12000 . #x1fffff)) (type ROM))
-    (memory dataRAM (address (#x200000 . #x37ffff)) (type RAM))
-    (memory Vector (address (#x0000 . #x03ff)))
-    ))
