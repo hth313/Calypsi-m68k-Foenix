6427925a010755080442ac064de176a498da1335
Håkan Thörngren <hth313@gmail.com>
2025-10-26 14:08:33 -0700

    Add missing headers

---
+++ include/foenix.h
@@ -0,0 +1,17 @@
+#ifndef __FOENIX_H__
+#define __FOENIX_H__
+
+#include "foenix/beatrix.h"
+#include "foenix/gavin.h"
+#include "foenix/interrupt.h"
+#include "foenix/joystick.h"
+#include "foenix/opl3.h"
+#include "foenix/psg.h"
+#include "foenix/rtc.h"
+#include "foenix/sid.h"
+#include "foenix/system.h"
+#include "foenix/timer.h"
+#include "foenix/uart.h"
+#include "foenix/vicky.h"
+
+#endif // __FOENIX_H__
---
+++ include/mcp.h
@@ -0,0 +1,9 @@
+#ifndef __MCP_H__
+#define __MCP_H__
+
+#include "mcp/errors.h"
+#include "mcp/interrupt.h"
+#include "mcp/types.h"
+#include "mcp/syscall.h"
+
+#endif // __MCP_H__
